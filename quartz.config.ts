5c1ccc8203b9cf87c555769b102e28b5e0b51aac
CaedesAeterna <ferenczgellert@gmail.com>
2026-04-15 14:48:18 +0300

    Quartz sync: Apr 15, 2026, 2:48 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "caedesaeterna.github.io/knowledge-base",
+    baseUrl: "CaedesAeterna.github.io/knowledge-base",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
